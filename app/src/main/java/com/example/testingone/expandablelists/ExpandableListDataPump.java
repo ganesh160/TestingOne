690ead18b0416b0da28772bca2bbf555b123358a
Munigala.Ganesh <Munigala.Ganesh@karvy.com>
2019-09-28 16:03:16 +0530

    java expandable listview code .

---
+++ app/src/main/java/com/example/testingone/expandablelists/ExpandableListDataPump.java
@@ -0,0 +1,47 @@
+package com.example.testingone.expandablelists;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class ExpandableListDataPump {
+
+    public static HashMap<String, List<String>> getData() {
+        HashMap<String, List<String>> expandableListDetail = new HashMap<String, List<String>>();
+
+        List<String> cricket = new ArrayList<String>();
+        cricket.add("India");
+        cricket.add("Pakistan");
+        cricket.add("Australia");
+        cricket.add("England");
+        cricket.add("South Africa");
+        cricket.add("Britan");
+
+        List<String> football = new ArrayList<String>();
+        football.add("Brazil");
+        football.add("Spain");
+        football.add("Germany");
+        football.add("Netherlands");
+        football.add("Italy");
+
+        List<String> basketball = new ArrayList<String>();
+        basketball.add("United States");
+        basketball.add("Spain");
+        basketball.add("Argentina");
+        basketball.add("France");
+        basketball.add("Russia");
+
+        List<String> volleyBall = new ArrayList<String>();
+        volleyBall.add("United States");
+        volleyBall.add("Spain");
+        volleyBall.add("Argentina");
+        volleyBall.add("France");
+        volleyBall.add("Russia");
+
+        expandableListDetail.put("CRICKET TEAMS", cricket);
+        expandableListDetail.put("FOOTBALL TEAMS", football);
+        expandableListDetail.put("BASKETBALL TEAMS", basketball);
+        expandableListDetail.put("VolleyBall TEAMS", volleyBall);
+        return expandableListDetail;
+    }
+}
